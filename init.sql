a9b11b08b861977e886cd5bb458c87797b2c96c0
kornyellow <kornkubzaza@gmail.com>
2024-10-22 01:02:32 +0700

    Update tables

--- init.sql
+++ init.sql
@@ -1,34 +1,58 @@
 CREATE SCHEMA IF NOT EXISTS public;
 
--- USER TABLES
 CREATE TABLE IF NOT EXISTS public."User" (
     "Id" VARCHAR(8) NOT NULL PRIMARY KEY,
     "Password" VARCHAR(64) NOT NULL,
     "Gender" VARCHAR(6) NOT NULL,
     "NameNick" VARCHAR(256) NOT NULL,
     "NameFirst" VARCHAR(256) NOT NULL,
-    "NameLast" VARCHAR(256) NOT NULL
+    "NameLast" VARCHAR(256) NOT NULL,
+    "Profile" VARCHAR(256) NOT NULL,
 );
 CREATE TABLE IF NOT EXISTS public."UserToken" (
     "Id" VARCHAR(64) NOT NULL PRIMARY KEY,
-    "UserId" VARCHAR(8) NOT NULL REFERENCES public."User"("Id"),
+    "UserId" VARCHAR(8) NOT NULL,
     "Created" TIMESTAMP NOT NULL,
-    "Expired" TIMESTAMP NOT NULL
+    "Expired" TIMESTAMP NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY ("UserId")
+        REFERENCES public."User" ("Id")
 );
 
--- CURRICULUM TABLES
-CREATE TABLE IF NOT EXISTS public."Curriculum" (
-    "Id" VARCHAR(4) NOT NULL,
-    "Year" VARCHAR(4) NOT NULL,
-    "Name" VARCHAR(256) NOT NULL,
-    "NameEng" VARCHAR(256) NOT NULL,
-    "Degree" VARCHAR(256) NOT NULL,
-    "DegreeShort" VARCHAR(128) NOT NULL,
-    "DegreeEng" VARCHAR(256) NOT NULL,
-    "DegreeEngShort" VARCHAR(128) NOT NULL
+CREATE TABLE public."Curriculum" (
+    "Id" SERIAL PRIMARY KEY,              
+    "UniqueId" VARCHAR(4) NOT NULL,     
+    "Year" VARCHAR(4) NOT NULL,            
+    "NameTh" VARCHAR(256),                
+    "NameEn" VARCHAR(256),                 
+    "DegreeNameTh" VARCHAR(256),            
+    "DegreeNameThShort" VARCHAR(128),        
+    "DegreeNameEn" VARCHAR(256),             
+    "DegreeNameEnShort" VARCHAR(128),         
 );
 
--- POPULATE DATA
+CREATE TABLE public."Transcript" (
+    "Id" SERIAL PRIMARY KEY,            
+    "UserId" VARCHAR(8) NOT NULL,           
+    "CurriculumId" INT NOT NULL,            
+    "Created" TIMESTAMP NOT NULL DEFAULT NOW(),
+    CONSTRAINT fk_curriculum
+        FOREIGN KEY ("CurriculumId")
+        REFERENCES public."Curriculum" ("Id"),
+    CONSTRAINT fk_user
+        FOREIGN KEY ("UserId")
+        REFERENCES public."User" ("Id"),
+);
+CREATE TABLE public."TranscriptData" (
+    "Id" SERIAL PRIMARY KEY,                 
+    "TranscriptId" INT NOT NULL,            
+    "SubjectId" VARCHAR(8) NOT NULL,        
+    "Grade" VARCHAR(4) NOT NULL,                    
+    CONSTRAINT fk_transcript
+        FOREIGN KEY ("TranscriptId")
+        REFERENCES public."Transcript" ("Id"),
+);
+
 INSERT INTO public."Curriculum" ("Id", "Year", "Name", "NameEng", "Degree", "DegreeShort", "DegreeEng", "DegreeEngShort") VALUES
 ('0155', '2565', 'หลักสูตรวิศวกรรมศาสตร์บัณฑิต สาขาวิชาวิศวกรรมคอมพิวเตอร์ (หลักสูตรนานาชาติ)', 'Bachelor of Engineering Program in Computer Engineering (International Program)', 'วิศวกรรมศาสตร์บัณฑิต (วิศวกรรมคอมพิวเตอร์)', 'วศ.บ. (วิศวกรรมคอมพิวเตอร์)', 'Bachelor of Engineering (Computer Engineering)', 'B.Eng. (Computer Engineering)'),
 ('0105', '2564', 'หลักสูตรวิศวกรรมศาสตร์บัณฑิต สาขาวิชาวิศวกรรมคอมพิวเตอร์', 'Bachelor of Engineering Program in Computer Engineering', 'วิศวกรรมศาสตร์บัณฑิต (วิศวกรรมคอมพิวเตอร์)', 'วศ.บ. (วิศวกรรมคอมพิวเตอร์)', 'Bachelor of Engineering (Computer Engineering)', 'B.Eng. (Computer Engineering)'),
